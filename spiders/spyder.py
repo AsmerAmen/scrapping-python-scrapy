0835c8ea3d5cb3716a4943db93a09928efc77002
AsmerAmen <asmer.amenar@gmail.com>
2020-04-19 19:07:21 +0200

    Added login to the basic spyder

--- spiders/spyder.py
+++ spiders/spyder.py
@@ -1,14 +1,30 @@
 import scrapy
+from scrapy.http import FormRequest
+from scrapy.utils.response import open_in_browser
+
 from ..items import SpyderUnoItem
 
 
 class QuoteSpyder(scrapy.Spider):
     name = 'spyder'
     start_urls = [
-        'http://quotes.toscrape.com/'
-        ]
+        # 'http://quotes.toscrape.com'
+        'http://quotes.toscrape.com/login'
+    ]
 
     def parse(self, response):
+        token = response.css('form input::attr(value)').extract_first()
+        # print(token)
+        return FormRequest.from_response(response,
+                                         formdata={
+                                            'csrf_token': token,
+                                            'username': 'asmer.amenar@gmail.com',
+                                            'password': 'password'
+                                         },
+                                         callback=self.start_scraping)
+
+    def start_scraping(self, response):
+        # open_in_browser(response)
         items = SpyderUnoItem()
 
         all_div_quotes = response.css('div.quote')
@@ -24,7 +40,7 @@ class QuoteSpyder(scrapy.Spider):
 
             yield items
 
-        next_page = response.css('li.next a::attr(href)').get()
-
-        if next_page:
-            yield response.follow(next_page, self.parse)
+        # next_page = response.css('li.next a::attr(href)').get()
+        #
+        # if next_page:
+        #     yield response.follow(next_page, self.parse)
